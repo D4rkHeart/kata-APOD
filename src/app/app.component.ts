6fb6ff02f9ce792b05daf10ee809fd26ade9956e
D4rkHeart <leonardo.surdez@gmail.com>
2023-12-06 16:37:07 +0100

    [Feat] LoadPrevious function + formatDate function - Format the date for the API. - Load the previous APOD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,4 +21,17 @@ export class AppComponent implements OnInit {
       this.apod = data;
     });
   }
+
+  loadPrevious() {
+    const previousDate = new Date(this.currentDate);
+    previousDate.setDate(previousDate.getDate() - 1);
+    this.currentDate = previousDate;
+    this.loadApod(this.formatDate(previousDate));
+  }
+  private formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
 }
