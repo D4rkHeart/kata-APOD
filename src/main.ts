09af86b690bd37fea7567a2c1d288ad2616de4a7
D4rkHeart <leonardo.surdez@gmail.com>
2023-12-06 16:06:08 +0100

    [Feat] Import of HttpClientModule

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppModule } from './app/app.module';
+import { importProvidersFrom } from '@angular/core';
+import {AppComponent} from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+  ]
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
