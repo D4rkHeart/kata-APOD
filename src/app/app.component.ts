fabfbd8c58518d3ea3b6ca939bc05feb85f80190
D4rkHeart <leonardo.surdez@gmail.com>
2023-12-06 16:34:26 +0100

    [Feat] Initialise the component + currentDate properties and load the APOD service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Kata-Apod';
+export class AppComponent implements OnInit {
+  apod: any;
+  currentDate: Date = new Date(); 
+
+  constructor(private apodService: ApodService) {}
+
+  ngOnInit() {
+    this.currentDate = new Date();
+    this.loadApod(this.formatDate(this.currentDate));
+  }
+
+  loadApod(date: string) {
+    this.apodService.getApod(date).subscribe(data => {
+      this.apod = data;
+    });
+  }
 }
